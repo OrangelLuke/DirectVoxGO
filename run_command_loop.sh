64765f40962bda341d83d9b775c295fa5727f0cf
OrangelLuke <angelator2001@gmail.com>
2023-07-10 15:33:38 +0200

    Result json in progress and bash script to execute

---
+++ run_command_loop.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Define the number of times to run the command
+num_times=5
+
+# Define the command to be executed
+command="python3 run.py --config configs/nerf/lego.py --render_test --eval_ssim --eval_lpips_vgg"
+
+# Loop to run the command multiple times
+for ((i=1; i<=$num_times; i++))
+do
+    echo "Running command: $command (Iteration: $i)"
+    $command
+done
