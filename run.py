cfb58dcc37d64557f802cba8c52f47e456d1a272
OrangelLuke <angelator2001@gmail.com>
2023-07-13 16:29:49 +0200

    Results and code from tests with the fine_render.num_voxels

--- run.py
+++ run.py
@@ -730,8 +730,8 @@ def execute_everything(args, cfg, device, data_dict):
 def load_results(l, ruta, cfg, results):
     execution = {}
     execution["config"] = {}
-    execution["config"]["coarse_render.num_voxels"] = cfg.coarse_model_and_render["num_voxels"]
-    execution["config"]["coarse_render.num_voxels_base"] = cfg.coarse_model_and_render["num_voxels_base"]
+    execution["config"]["fine_render.num_voxels"] = cfg.fine_model_and_render["num_voxels"]
+    execution["config"]["fine_render.num_voxels_base"] = cfg.fine_model_and_render["num_voxels_base"]
     execution["results"] = results
 
     l.append(execution)
@@ -749,8 +749,8 @@ def measure_memory_usage():
 def check_value_is_done(l, values):
     valuesDone = []
     for val in l:
-        if "coarse_render.num_voxels" in val["config"]:
-            valuesDone.append(val["config"]["coarse_render.num_voxels"])
+        if "fine_render.num_voxels" in val["config"]:
+            valuesDone.append(val["config"]["fine_render.num_voxels"])
     for val in values:
         if val not in valuesDone:
             return val
@@ -775,8 +775,9 @@ if __name__ == '__main__':
     #values = [1000, 5000, 10000]
     #values = [4096, 8192, 16384]
     #values = [1000, 20000, 40000]
-    values = [256000, 512000, 1024000, 2048000]
-    print("Original coarse_render.num_voxels ", cfg.coarse_model_and_render["num_voxels"])
+    #values = [256000, 512000, 1024000, 2048000]
+    values = [40**3, 80**3, 160**3, 320**3]
+    print("Original fine_render.num_voxels ", cfg.fine_model_and_render["num_voxels"])
 
     parent_dir = "logRecord"
 
@@ -797,9 +798,9 @@ if __name__ == '__main__':
         if value is None:
             print("All values have been used\n")
             break
-        cfg.coarse_model_and_render["num_voxels"] = value
-        cfg.coarse_model_and_render["num_voxels_base"] = value
-        print("## VALOR: ", cfg.coarse_model_and_render["num_voxels"], " ##")
+        cfg.fine_model_and_render["num_voxels"] = value
+        cfg.fine_model_and_render["num_voxels_base"] = value
+        print("## VALOR: ", cfg.fine_model_and_render["num_voxels"], " ##")
         # load images / poses / camera settings / data split
         data_dict = load_everything(args=args, cfg=cfg)
         try:
@@ -809,7 +810,7 @@ if __name__ == '__main__':
             memory_before = measure_memory_usage()
             execute_everything(args, cfg, device, data_dict)
             memory_after = measure_memory_usage()
-            directory = "coarse_render.num_voxels"+str(cfg.coarse_model_and_render["num_voxels"])
+            directory = "fine_render.num_voxels"+str(cfg.fine_model_and_render["num_voxels"])
             path = os.path.join(parent_dir, directory)
             print("El path para el directorio es: ", path)
             os.mkdir(path)
--- run_command_loop.sh
+++ run_command_loop.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Define the number of times to run the command
-num_times=5
+num_times=10
 
 # Define the command to be executed
 command="python3 run.py --config configs/nerf/lego.py --render_test --eval_ssim --eval_lpips_vgg"
