72e5124803d9e94805fbe76bd46d2dbab3104435
OrangelLuke <angelator2001@gmail.com>
2023-07-15 16:26:08 +0200

    Further tests for coarse_train.n_iters

--- run.py
+++ run.py
@@ -730,8 +730,9 @@ def execute_everything(args, cfg, device, data_dict):
 def load_results(l, ruta, cfg, results):
     execution = {}
     execution["config"] = {}
-    execution["config"]["fine_render.num_voxels"] = cfg.fine_model_and_render["num_voxels"]
-    execution["config"]["fine_render.num_voxels_base"] = cfg.fine_model_and_render["num_voxels_base"]
+    #execution["config"]["fine_render.num_voxels"] = cfg.fine_model_and_render["num_voxels"]
+    #execution["config"]["fine_render.num_voxels_base"] = cfg.fine_model_and_render["num_voxels_base"]
+    execution["config"]["coarse_train.n_iters"] = cfg.coarse_train["N_iters"]
     execution["results"] = results
 
     l.append(execution)
@@ -749,8 +750,8 @@ def measure_memory_usage():
 def check_value_is_done(l, values):
     valuesDone = []
     for val in l:
-        if "fine_render.num_voxels" in val["config"]:
-            valuesDone.append(val["config"]["fine_render.num_voxels"])
+        if "coarse_train.n_iters" in val["config"]:
+            valuesDone.append(val["config"]["coarse_train.n_iters"])
     for val in values:
         if val not in valuesDone:
             return val
@@ -776,8 +777,10 @@ if __name__ == '__main__':
     #values = [4096, 8192, 16384]
     #values = [1000, 20000, 40000]
     #values = [256000, 512000, 1024000, 2048000]
-    values = [40**3, 80**3, 160**3, 320**3]
-    print("Original fine_render.num_voxels ", cfg.fine_model_and_render["num_voxels"])
+    #values = [40**3, 80**3, 160**3, 320**3]
+    #values = [500, 20000]
+    values = [2048, 32768]
+    print("Original coarse_train.n_iters ", cfg.coarse_train["N_iters"])
 
     parent_dir = "logRecord"
 
@@ -798,9 +801,8 @@ if __name__ == '__main__':
         if value is None:
             print("All values have been used\n")
             break
-        cfg.fine_model_and_render["num_voxels"] = value
-        cfg.fine_model_and_render["num_voxels_base"] = value
-        print("## VALOR: ", cfg.fine_model_and_render["num_voxels"], " ##")
+        cfg.coarse_train["N_iters"] = value
+        print("## VALOR: ", cfg.coarse_train["N_iters"], " ##")
         # load images / poses / camera settings / data split
         data_dict = load_everything(args=args, cfg=cfg)
         try:
@@ -810,7 +812,7 @@ if __name__ == '__main__':
             memory_before = measure_memory_usage()
             execute_everything(args, cfg, device, data_dict)
             memory_after = measure_memory_usage()
-            directory = "fine_render.num_voxels"+str(cfg.fine_model_and_render["num_voxels"])
+            directory = "coarse_train.n_iters"+str(cfg.coarse_train["N_iters"])
             path = os.path.join(parent_dir, directory)
             print("El path para el directorio es: ", path)
             os.mkdir(path)
